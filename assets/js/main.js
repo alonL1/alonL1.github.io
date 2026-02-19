900e0ec6b5e85d4454236030eb1e849ee5cb1e57
alonL1 <alonlevy04@gmail.com>
2026-02-18 22:15:33 -0800

    second my role hidden on expand

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,10 @@ const closeCard = (card) => {
   if (expanded) {
     expanded.hidden = true;
   }
+  const rolePreview = card.querySelector('.project-role-preview');
+  if (rolePreview) {
+    rolePreview.hidden = false;
+  }
   const toggle = card.querySelector('[data-project-toggle]');
   if (toggle) {
     toggle.setAttribute('aria-expanded', 'false');
@@ -23,6 +27,10 @@ const openCard = (card) => {
   const expanded = card.querySelector('.project-expanded');
   if (expanded) {
     expanded.hidden = false;
+  }
+  const rolePreview = card.querySelector('.project-role-preview');
+  if (rolePreview) {
+    rolePreview.hidden = true;
   }
   const toggle = card.querySelector('[data-project-toggle]');
   if (toggle) {
@@ -61,5 +69,9 @@ projectCards.forEach((card) => {
   const expanded = card.querySelector('.project-expanded');
   if (expanded) {
     expanded.hidden = true;
+  }
+  const rolePreview = card.querySelector('.project-role-preview');
+  if (rolePreview) {
+    rolePreview.hidden = false;
   }
 });
